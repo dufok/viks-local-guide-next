923cb25475188c4245b97c890fe3675272a13285
Stepan Vladovskiy <duffokin@gmail.com>
2023-09-09 05:37:21 +0000

    feat: ver2

---
+++ README.md
@@ -0,0 +1,6 @@
+# viks-local-guide-next
+
+### Used on project
+- [Next.js](https://nextjs.org)
+- [T3 Stack](https://create.t3.gg/)
+- [Tailwind CSS](https://tailwindcss.com)
---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.mjs");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+
+  images: {
+    domains: ['lh5.googleusercontent.com'],
+  },
+};
+
+export default config;
+
